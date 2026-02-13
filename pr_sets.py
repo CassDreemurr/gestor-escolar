d4166a72cb9316d7320d8ca3b2e7a1bbca9753b5
Cassandra Morales <isis.mellado.morales@gmail.com>
2026-02-13 17:45:52 -0600

    Eliminando archivos que no pertenecen al gestor escolar

--- pr_sets.py
+++
@@ -1,9 +0,0 @@
-"""a = {1,2,3,4,5}
-a.remove(6)
-a.discard(6)
-print(a)"""
-#Remove: muestra un error si no existe el elemento especificado
-#Discard: no muestra error si no existe elemento especificado
-
-"""a = {1,2,3,4}
-b = {3,4,5,6}"""
